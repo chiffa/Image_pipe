d5c9178f7b6e212d1176805297b3bf8071c4064a
rfm110 <malikrubab@gmail.com>
2017-03-10 01:00:21 -0500

    Corrected user input for csv files

--- traversals.py
+++ traversals.py
@@ -89,40 +89,61 @@ def Linhao_traverse(main_root,
 
     for name_pattern in delset:
         del matched_images[name_pattern]
-    # count = 2
-    f1 = open("matched_images.csv", 'wb')
-    writer = csv.writer(f1, delimiter = '\t')
-    for key in matched_images:
-        writer.writerow([key]+ matched_images[key]+[0])
 
-    # for name_pattern, color_set in matched_images.iteritems():
-
-
-    # ask for user input before line 92, DO NOT OVERRIDE ORIGINAL CSV FILE UNTIL USER EXPLICITLY INPUTS FOR YOU TO DO SO
     user_input_about_new_csv_file = raw_input("If you would like to create a new csv file to write to, enter 1. Otherwise, enter 2")
+    f1 = open("matched_images.csv", 'wb')
     if user_input_about_new_csv_file == '1':
-        f2 = open("matched_images.csv", 'rb')
-        reader = csv.reader(f2, delimiter = '\t')
+
+        # this is the file we need to save unless user provides input saying we can override it
+        writer = csv.writer(f1, delimiter='\t')
+        for key in matched_images:
+            writer.writerow([key] + matched_images[key] + [0])
+        # f2 = open("matched_images.tmp", 'rb')
+        # reader_check = csv.reader(f1, delimiter = '\t')
+        # writer_check = csv.reader(f2, delimiter = '\t')
     else:
-         start_image = raw_input("Enter the name pattern (first column of csv file) of the image you would like to start with")
-        reader = csv.reader(f1, delimiter = '\t')
-        f3 = open("matched_images.tmp", 'wb')
-
-
-    for row in reader:
-        if start_image:
-            if row[0] != start_image:
+        start_image = raw_input("Enter the name pattern (first column of csv file) of the image you would like to start with")
+        reader_check = csv.reader(f1, delimiter = '\t')
+        f2 = open("matched_images.tmp", 'rb')
+        writer_check = csv.writer(f2, delimiter  = '\t')
+        for row in reader_check:
+            if str(row[0]) != start_image:
                 continue
-
-        # count -= 1
-        # name pattern = row[0]
-        # color set = ?
-        name_pattern = row[0]
-        color_set =  row[1]
-
+            name_pattern = row[0]
+            color_set = row[1]
         channels = []
         for color in color_set:
             channels.append(cf.tiff_stack_2_np_arr(color))
+
+    yield name_pattern, tags_dict[name_pattern], channels
+    for row in reader_check:
+        if row[0] == name_pattern:
+            row[2] = 1
+            writer_check.writerow(row)
+
+
+
+                    # for name_pattern, color_set in matched_images.iteritems():
+
+
+    # ask for user input before line 92, DO NOT OVERRIDE ORIGINAL CSV FILE UNTIL USER EXPLICITLY INPUTS FOR YOU TO DO SO
+
+
+    #
+    # for row in reader_check:
+    #     if start_image:
+    #         if row[0] != start_image:
+    #             continue
+    #
+    #     # count -= 1
+    #     # name pattern = row[0]
+    #     # color set = ?
+    #     name_pattern = row[0]
+    #     color_set =  row[1]
+    #
+    #     channels = []
+    #     for color in color_set:
+    #         channels.append(cf.tiff_stack_2_np_arr(color))
 
 
         # print 'starting to analyze', name_pattern
@@ -141,13 +162,13 @@ def Linhao_traverse(main_root,
         #         continue
         #     else:
         #         break
-
-        yield name_pattern, tags_dict[name_pattern], channels
-        f3 = open("matched_images.csv, 'rb")
-        reader2 = csv.reader(f3, delimiter = '\t')
-        for row in reader2:
-            if row[0] == name_pattern:
-                row[2] = 1
+        #
+        # yield name_pattern, tags_dict[name_pattern], channels
+        # f3 = open("matched_images.csv, 'rb")
+        # reader2 = csv.reader(f3, delimiter = '\t')
+        # for row in reader2:
+        #     if row[0] == name_pattern:
+        #         row[2] = 1
 
 
 
@@ -190,4 +211,91 @@ def Akshay_traverse(main_root):
         channels = np.split(stack, stack.shape[0])
         channels = [chan[0, :, :] for chan in channels]
 
+<<<<<<< Updated upstream
         yield name_pattern, group_by, channels
+=======
+        yield name_pattern, group_by, channels
+
+import numpy as np
+import core_functions as cf
+from collections import defaultdict
+import logging
+import os
+
+
+logger = logging.getLogger('Default Debug Logger')
+logger.setLevel(logging.DEBUG)
+
+fh = logging.FileHandler('debug_log.log')
+fh.setLevel(logging.DEBUG)
+
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+def Kristen_traverse(main_root,
+                    matching_rule='c', matching_map=None):
+
+    # main root = /run/user/1000/gvfs/smb-share:server=10.17.0.219,share=common/Users/kristen/Split GFP quant_Andrei/20170209
+
+    matched_images = defaultdict(lambda: [''] * len(matching_map.keys()))
+    tags_dict = defaultdict(lambda: [])
+
+    if matching_rule:
+        assert (matching_map is not None)
+
+    for current_location, sub_directories, files in os.walk(main_root):
+        if files:
+            for img in files:
+                # if ('.TIF' in img or '.tif' in img) and '_thumb_' not in img:
+                if ('.TIF' in img or '.tif' in img) and '_thumb_' not in img:
+                    prefix = cf.split_and_trim(current_location, main_root)
+                    img_codename = img.split(' ')[0].split('_')
+                    color = matching_map[img_codename[-1]]
+                    name_pattern = ' - '.join(prefix + img_codename[:-1])
+                    matched_images[name_pattern][color] = os.path.join(current_location, img)
+                    time_stamp = prefix[-1]
+
+                    # if time_stamp == 'HS':
+                    #     time = 0
+                    # elif 'HS' in time_stamp:
+                    #     time = -30
+                    # else:
+                    #     time = time_stamp[3:-3]  # int(time_stamp[3:-3])
+                    # tags_dict[name_pattern] = []
+                    # tags_dict[name_pattern].append(time)  # i dont think we need this commented out section above?
+                    _date = prefix[0][3:11]
+                    tags_dict[name_pattern].append("%s-%s-%s" % (_date[:2], _date[2:4], _date[4:]))
+                    tags_dict[name_pattern].append(prefix[-2])
+
+    delset = []
+    for name_pattern, (color_set) in matched_images.iteritems():
+        # debugger.logger.debug(color_set)
+        if any([color == '' for color in color_set]):
+            logger.info('in %s, colorset is broken:', name_pattern)
+            for color_name in color_set:
+                logger.info('\t %s', color_name)
+            logger.info('name_pattern will be deleted')
+            delset.append(name_pattern)
+
+    for name_pattern in delset:
+        del matched_images[name_pattern]
+
+    for name_pattern, color_set in matched_images.iteritems():
+        channels = []
+        for color in color_set:
+            channels.append(cf.tiff_stack_2_np_arr(color))
+
+        print 'starting to analyze', name_pattern
+
+        yield name_pattern, tags_dict[name_pattern], channels
+>>>>>>> Stashed changes
