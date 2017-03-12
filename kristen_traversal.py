3c46db2bdcaa43c3aafc780f37e2632aeca5ed19
Rubab MAlik <malikrubab@gmail.com>
2017-03-12 13:56:52 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	traversals.py
    Creating traversal script for Kristen

--- kristen_traversal.py
+++ kristen_traversal.py
@@ -11,14 +11,13 @@ def Kristen_traverse(main_root,
 
     matched_images = defaultdict(lambda: ['']*len(matching_map.keys()))
     tags_dict = defaultdict(lambda: [])
+#     using a dictionary instead of list since grouped by mutliple colors and may need to superimpose them
 #     provide matching map based on translator (a dictionary with color codes and associated colors/dyes)
 
     for current_location, sub_directories, files in os.walk(main_root):
         if files:
             for img in files:
                 if ('.TIF' in img or '.tif' in img) and '_thumb_' not in img:
-                    # prefix = cf.split_and_trim(current_location, main_root)
-                    # img_codename = img.split('.')[0].split(' ')
                     # img_codename = [img.split('.')[0]]   #linhao
                     prefix = cf.split_and_trim(current_location, main_root)
 
@@ -26,7 +25,6 @@ def Kristen_traverse(main_root,
                     name_pattern = ' - '.join(prefix + img_codename)
                     print "prefix", prefix
                     print "img codename", img_codename
-                    # name_pattern = ' - '.join([prefix[1]] + img_codename)
                     print "name pattern", name_pattern
                     group_by = img_codename[0][:2]
                     # grouping by color
@@ -34,10 +32,7 @@ def Kristen_traverse(main_root,
                     print
                     print
                     color = group_by
-                    print type(matched_images)
-                    matched_images[name_pattern][color] = os.path.join(current_location, img)
-                    # group_by = img_codename[0].split('rpe')[1].split('dapi')[0].strip()
-                    # matched_images.append((name_pattern, group_by, os.path.join(current_location, img)))
+                    # matched_images[name_pattern][color] = os.path.join(current_location, img)
 
     for name_pattern, group_by, image_location in matched_images:
         print "reached for loop"
@@ -46,7 +41,8 @@ def Kristen_traverse(main_root,
         stack = np.rollaxis(stack[0, :, :, :], 2)  # on the data where channels have not been split into z stacks
         channels = np.split(stack, stack.shape[0])
         channels = [chan[0, :, :] for chan in channels]
+#     dont forget to add the csv/user input portion after this part is running smoothly
 
-
-translator = {'C1':'DAPI', 'C3':'GFP', 'C4':'mCherry'}
+translator = {'C1':3, 'C3':0, 'C4':1}
+# C1 = DAPI, C2 = GFP, C3 = mCherry
 Kristen_traverse("/run/user/1000/gvfs/smb-share:server=10.17.0.219,share=common/Users/kristen/Split GFP quant_Andrei/", matching_map=translator)
